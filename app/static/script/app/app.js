2f0a04e97d98fc67cadf1c2df2bc82fe03ccc56d
Matt Priour <mpriour@gmail.com>
2011-10-04 11:28:13 -0400

    replace default app.js with demo.js contents for proper static-war builds

--- app/static/script/app/app.js
+++ app/static/script/app/app.js
@@ -1,94 +1,44 @@
-// this is where your application code goes
-
-var app;
-Ext.onReady(function() {
-    app = new gxp.Viewer({
-        portalConfig: {
-            layout: "border",
-            region: "center",
-            
-            // by configuring items here, we don't need to configure portalItems
-            // and save a wrapping container
-            items: [{
-                id: "centerpanel",
-                xtype: "panel",
-                layout: "fit",
-                region: "center",
-                border: false,
-                items: ["mymap"]
-            }, {
-                id: "westpanel",
-                xtype: "container",
-                layout: "fit",
-                region: "west",
-                width: 200
-            }],
-            bbar: {id: "mybbar"}
-        },
-        
-        // configuration of all tool plugins for this application
-        tools: [{
-            ptype: "gxp_layertree",
-            outputConfig: {
-                id: "tree",
-                border: true,
-                tbar: [] // we will add buttons to "tree.bbar" later
-            },
-            outputTarget: "westpanel"
+Ext.ns('app');
+app.demos = [{
+    title: 'Hurricane Irene Track',
+    url: 'irene.html',
+    description: 'Tracked Object Time Animation example: The storm position,' +
+    'NexRad Radar, and storm wind classifications will change along with the time. Starts at Aug 17, 2011 and continues' +
+    'until Aug 29, 2011 or until you stop it'
+}/*,{
+    title: '',
+    url: '',
+    description: ''
+}*/
+]
+app.ds = new Ext.data.Store({
+    reader: new Ext.data.JsonReader({
+        fields: ['title', 'url', 'description']
+    }),
+    proxy: new Ext.data.MemoryProxy(app.demos),
+    autoLoad: true
+})
+Ext.onReady(function(){
+    app.grid = new Ext.grid.GridPanel({
+        columns: [{
+            header: 'Title',
+            dataIndex: 'title',
+            width:150
         }, {
-            ptype: "gxp_addlayers",
-            actionTarget: "tree.tbar"
+            header: 'Link',
+            dataIndex: 'url',
+            tpl: '<a href="{url}">{url}</a>',
+            xtype: 'templatecolumn',
+            width:150
         }, {
-            ptype: "gxp_removelayer",
-            actionTarget: ["tree.tbar", "tree.contextMenu"]
-        }, {
-            ptype: "gxp_zoomtoextent",
-            actionTarget: "map.tbar"
-        }, {
-            ptype: "gxp_zoom",
-            actionTarget: "map.tbar"
-        }, {
-            ptype: "gxp_navigationhistory",
-            actionTarget: "map.tbar"
+            header: 'Description',
+            dataIndex: 'description',
+            width:500
         }],
-        
-        // layer sources
-        sources: {
-            local: {
-                ptype: "gxp_wmscsource",
-                url: "/geoserver/wms",
-                version: "1.1.1"
-            },
-            osm: {
-                ptype: "gxp_osmsource"
-            }
-        },
-        
-        // map and layers
-        map: {
-            id: "mymap", // id needed to reference map in portalConfig above
-            title: "Map",
-            projection: "EPSG:900913",
-            units: "m",
-            maxResolution: 156543.0339,
-            maxExtent: [-20037508, -20037508, 20037508, 20037508],
-            center: [-10764594.758211, 4523072.3184791],
-            zoom: 3,
-            layers: [{
-                source: "osm",
-                name: "mapnik",
-                group: "background"
-            }, {
-                source: "local",
-                name: "usa:states",
-                selected: true
-            }],
-            items: [{
-                xtype: "gx_zoomslider",
-                vertical: true,
-                height: 100
-            }]
-        }
-
-    });
-});
+        ds:app.ds,
+        stripeRows:true,
+        width: 850,
+        height:500,
+        renderTo: 'demogrid'
+    })
+})
